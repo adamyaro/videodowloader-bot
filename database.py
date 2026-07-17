3a7da69b6ee1b18dfba1023b2934a9a86fb045f8
adamyaro <adamys27@icloud.com>
2026-07-17 12:47:38 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,22 @@
-.
+import os
+import asyncpg
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+pool = None
+
+
+async def connect_db():
+    global pool
+
+    pool = await asyncpg.create_pool(DATABASE_URL)
+
+    await pool.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            telegram_id BIGINT UNIQUE,
+            username TEXT,
+            first_name TEXT,
+            joined_at TIMESTAMP DEFAULT NOW()
+        );
+    """)
